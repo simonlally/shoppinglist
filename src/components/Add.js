b88d10ab227ff65ae1cc447f5b7b1a48ead996be
Simon Lally <simonlallydev@gmail.com>
2019-06-05 12:17:59 -0400

    first commit

---
+++ src/components/Add.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Add extends React.Component {
+    render() {
+        return (
+            <div className="ListMain">
+                <div className="heading">
+                    <form>
+                        <input>
+                        </input>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Add;
